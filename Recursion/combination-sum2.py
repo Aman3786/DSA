da2793f0c9b990b2368b47b4cefaa308fecc011b
A.S <shaikhamaan3786@gmail.com>
2022-09-06 16:55:01 +0530

    Add files via upload

---
+++ Recursion/combination-sum2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solve(self,idx,cand,t,st,ans):
+        if t == 0:
+            ans.append(st[:])
+            return
+        
+        if t < 0: return
+        
+        for i in range(idx,len(cand)):
+            if i > idx and cand[i] == cand[i-1]: continue
+            st.append(cand[i])
+            self.solve(i+1,cand,t-cand[i],st,ans)
+            st.pop()
+
+        
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        st = []
+        candidates.sort()
+        self.solve(0,candidates,target,st,ans)
+        return ans
+        
