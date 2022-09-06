a5720f241b8c6ed2fd972c4ac96225daaa89f98c
A.S <shaikhamaan3786@gmail.com>
2022-09-06 17:51:44 +0530

    find a pair of 2 number sum equal to target. Here input array is Sorted.

---
+++ Arrays/Two-Sum2.py
@@ -0,0 +1,16 @@
+'''
+Two-Sum2:- find a pair of 2 number sum equal to target. Here input array is Sorted. Return the Index.
+'''
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers)-1
+        
+        while i < j:
+            if numbers[i] + numbers[j] == target:
+                return [i+1,j+1]
+            elif numbers[i] + numbers[j] > target:
+                j-=1
+            else:
+                i+=1
